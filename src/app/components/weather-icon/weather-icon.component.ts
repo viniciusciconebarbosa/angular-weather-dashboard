c9e839584a29dda8d8af1323385fe4816af37acd
vinicius <viniciuscicone@gmail.com>
2025-09-06 19:44:07 -0300

    refactor: separate component files (current-weather, weather-icon) into HTML, SCSS and TS

--- src/app/components/weather-icon/weather-icon.component.ts
+++ src/app/components/weather-icon/weather-icon.component.ts
@@ -5,33 +5,8 @@ import { CommonModule } from '@angular/common';
   selector: 'app-weather-icon',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div class="weather-icon" [style.width.px]="size" [style.height.px]="size">
-      <img [src]="iconUrl" [alt]="condition" [style.width.px]="size" [style.height.px]="size">
-    </div>
-  `,
-  styles: [`
-    .weather-icon {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-    
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-      color: inherit;
-    }
-
-    :host-context(.current-weather) img {
-      color: white;
-    }
-
-    :host-context(.forecast-day) img {
-      color: var(--text-color);
-    }
-  `]
+  templateUrl: './weather-icon.component.html',
+  styleUrls: ['./weather-icon.component.scss']
 })
 export class WeatherIconComponent {
   @Input() condition: string = '';
